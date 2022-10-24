a3df7b4c9a5e0a719a73d53e6fa018278a890414
peterkuchar <peterkucharvt@gmail.com>
2022-10-24 16:12:17 +0200

    Hello added

--- Harvard/python/hello.py
+++ Harvard/python/hello.py
@@ -1,1 +1,8 @@
-print("hello")
+# Ask user for name and age
+first_name = input("What's your firs name? ")
+last_name = input("What's your last name? ")
+age = input("What's your age? ")
+greet = "Hello,"
+
+# Say hello to user
+print(greet, first_name, last_name, "(" + age + ")yo.")
