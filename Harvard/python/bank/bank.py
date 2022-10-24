920fdfb27e4bc3a0233a747af17dfb674b693f7e
peterkuchar <peterkucharvt@gmail.com>
2022-10-24 18:22:55 +0200

    bank submited with 100%

---
+++ Harvard/python/bank/bank.py
@@ -0,0 +1,9 @@
+greeting = input("Greeting: ").strip().lower()
+
+if greeting.stratswith('h') == True:
+    if greeting.startswith('hello') == True:
+        print("$0")
+    else:
+        print("$20")
+else:
+    print("$0")
