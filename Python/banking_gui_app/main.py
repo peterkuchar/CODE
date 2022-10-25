b91621c271e1b69178f1b04e0b350556e3dcf88c
peterkuchar <peterkucharvt@gmail.com>
2022-10-25 15:43:05 +0200

    secont part done

--- Python/banking_gui_app/main.py
+++ Python/banking_gui_app/main.py
@@ -1,11 +1,12 @@
 from cProfile import label
 from tkinter import *
 import os
+from unicodedata import name
 from PIL import ImageTk, Image
 
 # Main Screen
 master = Tk()
-master.title('Banking App')
+master.title('George')
 
 # Functions
 def finish_reg():
@@ -17,12 +18,12 @@ def finish_reg():
     all_accounts = os.listdir()
     
     if name == "" or city == "" or country == "" or password == "":
-        notif.config(fg="red", text="All fields required!")
+        notif.config(fg="red", text="Všetky polia povinné!")
         return
     
     for name_check in all_accounts:
         if name == name_check:
-            notif.config(fg="red", text="Account already exists!")
+            notif.config(fg="red", text="Účet už existuje!")
             return
         else:
             new_file = open(name,"w")
@@ -30,8 +31,9 @@ def finish_reg():
             new_file.write(password+'\n')
             new_file.write(city+'\n')
             new_file.write(country+'\n')
+            new_file.write('0')
             new_file.close()
-            notif.config(fg="green", text="Acoount has been created.")
+            notif.config(fg="green", text="Účet bol vytvorený.")
     
 def register():
     # Variables
@@ -52,11 +54,11 @@ def register():
     register_screen.title('Register')
     
     # Labels    
-    Label(register_screen, text="Please enter your details below to register", font=("Calibri", 12)).grid(row=0,sticky=N,pady=10)
-    Label(register_screen, text="Name", font=("Calibri", 12)).grid(row=1,sticky=W)
-    Label(register_screen, text="City", font=("Calibri", 12)).grid(row=2,sticky=W)
-    Label(register_screen, text="Country", font=("Calibri", 12)).grid(row=3,sticky=W)
-    Label(register_screen, text="Password", font=("Calibri", 12)).grid(row=4,sticky=W)
+    Label(register_screen, text="Prosím zadajte informácie pre registráciu.", font=("Calibri", 12)).grid(row=0,sticky=N,pady=10)
+    Label(register_screen, text="Meno", font=("Calibri", 12)).grid(row=1,sticky=W)
+    Label(register_screen, text="Mesto", font=("Calibri", 12)).grid(row=2,sticky=W)
+    Label(register_screen, text="Krajina", font=("Calibri", 12)).grid(row=3,sticky=W)
+    Label(register_screen, text="Heslo", font=("Calibri", 12)).grid(row=4,sticky=W)
     notif = Label(register_screen, font=("Calibri", 12))
     notif.grid(row=6, sticky=N, pady=10)
     
@@ -67,10 +69,98 @@ def register():
     Entry(register_screen,textvariable=temp_password,show="*").grid(row=4, column=0)
     
     # Buttons
-    Button(register_screen, text="Register", command=finish_reg, font=('Calibri', 12)).grid(row=5,sticky=N,pady=10)
+    Button(register_screen, text="Registrovať", command=finish_reg, font=('Calibri', 12)).grid(row=5,sticky=N,pady=10)
+
+def login_session():
+        all_accounts = os.listdir()
+        login_name = temp_login_name.get()
+        login_password = temp_login_password.get()
+        
+        for name in all_accounts:
+            if name == login_name:
+                file = open(name, "r")
+                file_data = file.read()
+                file_data = file_data.split('\n')
+                password = file_data[1]
+                
+                # Account Dashboard
+                if login_password == password:
+                    login_screen.destroy()
+                    account_dashboard = Toplevel(master)
+                    account_dashboard.title('Dashboard')
+                    
+                    # Labels
+                    Label(account_dashboard, text="Osobný účet", font=("Calibri", 12)).grid(row=0, sticky=N, padx=10)
+                    Label(account_dashboard, text="Vitaj " + name, font=("Calibri", 12)).grid(row=1, sticky=N, pady=5)
+                    
+                    # Buttons
+                    Button(account_dashboard, text="Inoformácie", font=("Calibri", 12), width=30, command=personal_details).grid(row=2, sticky=N, padx=10)
+                    Button(account_dashboard, text="Vložiť", font=("Calibri", 12), width=30, command=deposit).grid(row=3, sticky=N, padx=10)
+                    Button(account_dashboard, text="Vybrať", font=("Calibri", 12), width=30, command=withdraw).grid(row=4, sticky=N, padx=10)
+                    Button(account_dashboard, text="Nová platba", font=("Calibri", 12), width=30, command=send).grid(row=5, sticky=N, padx=10)
+                    Label(account_dashboard).grid(row=5, sticky=N, pady=10)
+                    return
+                else:
+                    login_notif.config(fg="red", text="Heslo neplatné!")
+                    return
+        login_notif.config(fg="red", text="Účet neplatný!")
+        
+def deposit():
+    print("deposit")
+
+def withdraw():
+    print("withdraw")
     
+def personal_details():
+    # Variables
+    file = open(login_name, 'r')
+    file_data = file.read()
+    user_details = file_data.split('\n')
+    details_name = user_details[0]
+    details_city = user_details[2]
+    details_country = user_details[3]
+    details_balance = user_details[4]
+    
+    # Personal details screen
+    personal_details_screen = Toplevel(master)
+    personal_details_screen.title("Účet " + name)
+    
+    # Labels
+    Label(personal_details_screen, text="Osobný účet", font=("Calibri", 12)).grid(row=0, sticky=N, padx=10)
+    Label(personal_details_screen, text="Meno: " + details_name, font=("Calibri", 12)).grid(row=1, sticky=W, padx=10)
+    Label(personal_details_screen, text="Mesto: " + details_city, font=("Calibri", 12)).grid(row=2, sticky=W, padx=10)
+    Label(personal_details_screen, text="Krajina: " + details_country, font=("Calibri", 12)).grid(row=3, sticky=W, padx=10)
+    Label(personal_details_screen, text="Zostatok na účte: €" + details_balance, font=("Calibri", 12)).grid(row=4, sticky=W, padx=10)
+    
+def send():
+    print("send")
+        
 def login():
-    print("This is a login page")
+    # Variables
+    global temp_login_name
+    global temp_login_password
+    global login_notif
+    global login_screen
+    temp_login_name = StringVar()
+    temp_login_password = StringVar()
+    
+    # Login Screen
+    login_screen = Toplevel(master)
+    login_screen.title('Log in')
+    
+    # Labels
+    Label(login_screen, text="Prihlásenie", font=('Calibri', 12)).grid(row=0, sticky=N, pady=10)
+    Label(login_screen, text="Meno", font=('Calibri', 12)).grid(row=1, sticky=W)
+    Label(login_screen, text="Heslo", font=('Calibri', 12)).grid(row=2, sticky=W)
+    login_notif = Label(login_screen, font=('Calibri', 12))
+    login_notif.grid(row=4, sticky=N)
+    
+    # Entry
+    Entry(login_screen, textvariable=temp_login_name).grid(row=1, column=1, padx=5)
+    Entry(login_screen, textvariable=temp_login_password, show="*").grid(row=2, column=1, padx=5)
+    
+    # Button
+    Button(login_screen, text="Prihlásenie", command=login_session, width=15, font=('Calibri', 12)).grid(row=3, sticky=N, pady=5, padx=5)
 
 # Image Import
 img = Image.open('george.png')
@@ -83,7 +173,7 @@ Label(master, text="George", font=("Calibri",14)).grid(row=1, sticky=N, pady=10)
 Label(master, image=img).grid(row=2,sticky=N,pady=15)
 
 # Buttons
-Button(master, text="Register", font=("Calibri", 12), width=20, command=register).grid(row=3,sticky=N)
-Button(master, text="Login", font=("Calibri", 12), width=20, command=login).grid(row=4,sticky=N,pady=10)
+Button(master, text="Registrovať", font=("Calibri", 12), width=20, command=register).grid(row=3,sticky=N)
+Button(master, text="Prihlásiť", font=("Calibri", 12), width=20, command=login).grid(row=4,sticky=N,pady=10)
 
 master.mainloop()
