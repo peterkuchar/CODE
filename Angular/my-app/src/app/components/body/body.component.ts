702bc0c4b793a33f4927f411bcad44af43515a96
peterkuchar <peterkucharvt@gmail.com>
2022-12-10 19:06:25 +0100

    niekde som sa posunul no

--- Angular/my-app/src/app/components/body/body.component.ts
+++ Angular/my-app/src/app/components/body/body.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-body',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./body.component.css']
 })
 export class BodyComponent {
-  button: string = 'Add'
+
+  button: string = 'Add';
+
+  taskList: any[] = []
+  newTaskForm = this.formBuilder.group({
+    taskItem: '',
+    taskDate: '',
+    taskTime: ''
+  })
+
+  constructor(
+  private formBuilder: FormBuilder
+  ) {}
+
+  addTask() {
+    const value = this.newTaskForm.value.taskItem
+    this.taskList.push({ id: this.taskList.length, name: value })
+    this.newTaskForm.reset();
+  }
+
+
+  // task: string = document.getElementById('inputText');
+
 }
--- Angular/my-app/src/app/components/header/header.component.ts
+++ Angular/my-app/src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'My app';
+  title: string = 'TASKS';
   button: string = 'Add'
 
   constructor() {}
